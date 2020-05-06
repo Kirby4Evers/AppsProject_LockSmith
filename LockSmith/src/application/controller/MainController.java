3afe5ae31799eda11f02e54c6ebf6bc2f999c092
14132 <14132@NASA.attlocal.net>
2020-05-06 17:57:03 -0500

    controller updated for incorrect password

--- LockSmith/src/application/controller/MainController.java
+++ LockSmith/src/application/controller/MainController.java
@@ -1,10 +1,11 @@
 package application.controller;
 
-import java.io.File;
 import java.io.IOException;
 
 import application.model.Model;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 public class MainController {
@@ -12,35 +13,46 @@ public class MainController {
 	private TextField inputField1;
 	@FXML
 	private TextField inputField2;
-	
-	//Creating object of model class
-	 Model model = new Model();
 
-	
-	 
+	// Creating object of model class
+	Model model = new Model();
+
 	/**
 	 * called when sign up button is pressed
 	 */
-	 @FXML
-	public void handle1() {
-		String username = inputField1.getText(); 
-		 String masterPassword = inputField2.getText();
-		 model.signUp(username, masterPassword);
-	 }
+	@FXML
+	public void handle2() {
+
+		String username = inputField1.getText();
+		String masterPassword = inputField2.getText();
+
+		model.signUp(username, masterPassword);
+	}
+
 	/**
 	 * called when sign in button is pressed
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
-	 @FXML
-	public void handle2() throws IOException {
-		String username = inputField1.getText(); 
-		 String password = inputField2.getText();
-		 boolean isMatch=model.login(username, password);
-		 
-		 // if match, should go to next page.
-		 
-		 // TODO: if (isMatch)go to next page
-		 
+	@FXML
+	public void handle1() throws IOException {
+		String username = inputField1.getText();
+		String password = inputField2.getText();
+		boolean isMatch = model.login(username, password);
+		// if match, should go to next page.
+		// TODO: if (isMatch)go to next page
+		if (isMatch)
+			System.out.println("correct password");
+		// if password is not match, dialog box will appear with message.
+		else {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setContentText("Incorrect Password!!");
+			alert.show();
+		}
+	
+
+		
+
 	}
 
 }
