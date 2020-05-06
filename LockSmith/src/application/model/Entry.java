b1391341c4ddbb5f787e5bd0ef5fd940399cc504
SomeGuyNamedJon <60898789+SomeGuyNamedJon@users.noreply.github.com>
2020-05-06 13:54:40 -0500

    Create Entry.java
    Basic Entry class for storing password entries with empty methods for encryption and decryption to be created later

---
+++ LockSmith/src/application/model/Entry.java
@@ -0,0 +1,56 @@
+#package application.model
+public class Entry{
+  private String website;
+  private String username;
+  private String password;
+  
+  public Entry(){
+    website = null;
+    username = null;
+    password = null;
+  }
+  
+  public Entry(String website, String username, String password){
+    this.website = website;
+    this.username = username;
+    //TODO: run entered password string through encryption method
+    this.password = password;
+  }
+  
+  public String getWebsite(){
+    return this.website;
+  }
+  
+  public String getUsername(){
+    return this.username;
+  }
+  
+  public String getPassword(){
+    return this.password;
+  }
+  
+  public String getPasswordDecrypted(){
+    //TODO: add method of decryption
+    return this.password;
+  }
+  
+  public void setWebsite(String website){
+    this.website = website;
+  }
+  
+  public void setUsername(String username){
+    this.username = username;
+  }
+  
+  public void setPassword(String password){
+    //TODO: run through encryption method
+    this.password = password;
+  }
+  
+  //TODO: complete Encryption and Decryption methods
+  private void Encrypt(){
+  }
+  private void Decrypt(){
+  }
+  
+}
