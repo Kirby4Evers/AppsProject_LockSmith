71c1a2be4697782cf757c7e2d94c8cc70820f6f8
14132 <14132@NASA.attlocal.net>
2020-05-07 16:33:11 -0500

    removed changes

--- LockSmith/src/application/controller/Controller2.java
+++ LockSmith/src/application/controller/Controller2.java
@@ -7,17 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Controller2 extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/showEntries.fxml"));
-			Scene scene = new Scene(root,600,600);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("LockSmith");
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
 
 }
