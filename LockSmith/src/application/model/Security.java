c05f9691f9e850664dabb32a982561229568e5e5
Kyle E <riy335@my.utsa.edu>
2020-05-08 16:24:38 -0500

    Finally fixed it

--- LockSmith/src/application/model/Security.java
+++ LockSmith/src/application/model/Security.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.UnsupportedEncodingException;
+import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -56,16 +57,40 @@ public abstract class Security {
 	 * based on a simple "blowfish" example described here
 	 * https://stackoverflow.com/questions/5244950/encryption-with-blowfish-in-java
 	 */
-	public static String encryptS(String text, String key) throws Exception
+	public static String encryptS(String text, String hashedP) throws Exception
 	{
 		String strData="";
+		//making the key 128 bytes
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(hashedP.getBytes());
+        byte[] mdbytes = md.digest();
+        byte[] key = new byte[mdbytes.length / 2];
+        
+        for(int I = 0; I < key.length; I++){
+            // Choice 1 for using only 128 bits of the 256 generated
+        	key[I] = mdbytes[I];
+        }
 		
 		try {
-			SecretKeySpec skeyspec=new SecretKeySpec(key.getBytes(),"Blowfish");
-			Cipher cipher=Cipher.getInstance("Blowfish");
+				
+            // Create key and cipher
+            Key aesKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            // encrypt the text
+            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            byte[] encrypted = cipher.doFinal(text.getBytes());
+            strData = new String(encrypted);
+            //System.err.println(new String(encrypted));
+            // decrypt the text
+           
+			/*
+			SecretKeySpec skeyspec = new SecretKeySpec(key.getBytes(),"Blowfish");
+			
+			Cipher cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
 			cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
-			byte[] encrypted=cipher.doFinal(text.getBytes("UTF-8"));
+			byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
 			strData=new String(encrypted);
+			*/
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -80,16 +105,38 @@ public abstract class Security {
 	 * based on a simple "blowfish" example described here
 	 * https://stackoverflow.com/questions/5244950/encryption-with-blowfish-in-java
 	 */
-	public static String decryptS(String text, String key) throws Exception
+	public static String decryptS(String text, String hashedP) throws Exception
 	{
+		
 		String strData="";
+		//making the key 128 bytes
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(hashedP.getBytes());
+        byte[] mdbytes = md.digest();
+        byte[] key = new byte[mdbytes.length / 2];
+        
+        for(int I = 0; I < key.length; I++){
+            // Choice 1 for using only 128 bits of the 256 generated
+        	key[I] = mdbytes[I];
+        }
 		
 		try {
+			
+			Key aesKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+			
+			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			String decrypted = new String( cipher.doFinal(text.getBytes()) ) ;
+			strData = new String(decrypted);
+	        //System.err.println(decrypted);
+				
+			/*
 			SecretKeySpec sKeySpecs=new SecretKeySpec(key.getBytes(),"Blowfish");
-			Cipher cipher=Cipher.getInstance("Blowfish");
+			Cipher cipher=Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
 			cipher.init(Cipher.DECRYPT_MODE, sKeySpecs);
 			byte[] decrypted=cipher.doFinal(text.getBytes());
 			strData=new String(decrypted);
+			*/
 			
 		} catch (Exception e) {
 			e.printStackTrace();
